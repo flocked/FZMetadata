7c91865e04fcb79daa69154d999ea9df46a31cf1
Florian Zand <florianzand@me.com>
2025-08-21 22:06:30 +0200

    Update MetadataItem.swift

--- Sources/FZMetadata/MetadataItem/MetadataItem.swift
+++ Sources/FZMetadata/MetadataItem/MetadataItem.swift
@@ -1425,3 +1425,13 @@ extension MetadataItem {
         }
     }
 }
+
+extension MetadataItem: Hashable {
+    public static func == (lhs: MetadataItem, rhs: MetadataItem) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
